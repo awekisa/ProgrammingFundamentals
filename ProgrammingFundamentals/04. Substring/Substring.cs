daedd074b9f352940b044bba13b4c141da6fba88
awekisa <dimitar.valtchev@gmail.com>
2016-05-24 14:24:40 +0300

    GitHub and Debugging

---
+++ ProgrammingFundamentals/04. Substring/Substring.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Substring
+{
+    public static void Main()
+    {
+        string text = Console.ReadLine();
+        int jump = int.Parse(Console.ReadLine());
+
+        const char Search = 'p';
+        bool hasMatch = false;
+        char[] compare = text.ToCharArray();
+        string matchedString;
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (compare[i] == Search)
+            {
+                hasMatch = true;
+
+                int endIndex = jump+1;
+
+                if (endIndex + i >= text.Length)
+                {
+                    matchedString = text.Substring(i);
+                    Console.WriteLine(matchedString);
+                    break;
+                }
+
+                matchedString = text.Substring(i, endIndex);
+                Console.WriteLine(matchedString);
+                i += endIndex;
+            }
+        }
+
+        if (!hasMatch)
+        {
+            Console.WriteLine("no");
+        }
+    }
+}
